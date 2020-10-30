02735f671f7929970bb3b569c3ab89a94f582d86
bobbylin <mr.linjiahao@gmail.com>
2020-10-30 16:22:18 +0800

    Added Chapter 5 - High order functions

---
+++ chapter5_higher_order_functions.js
@@ -0,0 +1,39 @@
+/*
+Explore the higher order functions that are available for arrays
+- reduce, filter, map, forEach etc.
+*/
+
+// Flattening
+let arrays = [[1, 2, 3], [4, 5], [6]];
+//console.log(arrays.reduce((a, b) => a.concat(b),[])); // Initial array should be empty
+//console.log(arrays.reduce((a, b) => a.concat(b),[0, 1, 2, 3, 4, 5, 6])); // You can add additional items initially
+
+// Your Own Loop
+function loop(cur_val, test, update, body) {
+    while(test(cur_val)) {
+        body(cur_val);
+        cur_val = update(cur_val);
+    }
+}
+
+// loop(3, n => n > 0, n => n - 1, console.log);
+
+// Everything
+function every(array, predicate) {
+    for (let i = 0; i < array.length; i++) {
+        if(!predicate(array[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function every_using_some(array, predicate) {
+    // There is some element which does not meet predicate 
+    // -> If some returns true, then we need to return false for every
+    return !array.some(element => !predicate(element));
+}
+
+console.log(every([1, 3, 5], n => n < 10));
+console.log(every([2, 4, 16], n => n < 10));
+console.log(every([], n => n < 10));
